023ddd66c170cc01698d7026c1408d678129a945
SigurdsShootingRange <AIRORUDO@gmail.com>
2016-12-11 15:00:55 +0300

    Cleaned up some stuff

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -7,9 +7,7 @@ using System.Windows;
 
 namespace WpfApplication1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         bool isFirstMessage = true;
